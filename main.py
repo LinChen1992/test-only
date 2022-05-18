87a7636054dc4462e213e552dcb6df5cc5ff0556
Zlata Izvalava <zlata.izvalava@gmail.com>
2022-05-18 12:34:54 -0600

    fix: add docstring for divide function

--- main.py
+++ main.py
@@ -7,4 +7,14 @@ def add(num1, num2):
 
 
 def divide(num1, num2):
+    """
+    Divide two numbers provided as argments.
+
+    Args:
+        num1 (_type_): _description_
+        num2 (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return num1 / num2
